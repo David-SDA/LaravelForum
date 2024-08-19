3cc554c9ee00b20d0c6419bdc855f71263a1daa9
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-19 16:37:57 +0200

    Tests de modifications

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Laravel Forum</title>
+        <meta name="description" content="Forum to learn Laravel for myself">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>Laravel Forum</h1>
+    </body>
+</html>
