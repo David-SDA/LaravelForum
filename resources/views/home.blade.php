b2f167110f46af8e74e32a0f4160168313432ccc
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-19 16:59:44 +0200

    Modifications de l'emplacement du css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,14 @@
         <title>Laravel Forum</title>
         <meta name="description" content="Forum to learn Laravel for myself">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <h1>Laravel Forum</h1>
+        <main>
+            <h1>Laravel Forum</h1>
+        </main>
     </body>
 </html>
