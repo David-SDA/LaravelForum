79c72279b110520cc433fff47334d5bb37a65892
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-20 16:00:14 +0200

    Test d'une modification d'une table

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -43,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Verify if user has un certain role
+     * 
+     * @param string $role
+     * @return bool
+     */
+    public function hasRole(string $role): bool{
+        return $this->role === $role;
     }
 }
