88631b14be8fe2dc35a768ffc3e467b8bda696b9
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-22 17:02:31 +0200

    Ajout de la base du front de la page d'inscription

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -4,4 +4,40 @@
 
 @section('content')
     <h2>Register</h2>
+
+    @if($errors->any())
+        <div>
+            <p>Something went wrong !</p>
+            <ul>
+                @foreach($errors->all() as $errors)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="/register" method="post">
+        @csrf
+
+        <div>
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" value="{{ old('name') }}" autofocus>
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" value="{{ old('password') }}">
+        </div>
+        <div>
+            <label for="password_confirmation">Password Confirmation</label>
+            <input type="password" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}">
+        </div>
+        <div>
+            <button>Register</button>
+        </div>
+    </form>
+
 @endsection
