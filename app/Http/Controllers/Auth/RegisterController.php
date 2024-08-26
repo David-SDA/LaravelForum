190ec86f380f5a497b34ef15a0df1e8be272f47e
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-26 11:53:22 +0200

    Correction d'un problème d'inscription

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
