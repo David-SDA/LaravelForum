c771f5eba25e2da1e54fe1bdadc0fee766d74435
David Sousa de Araujo <davidaraujo2001@gmail.com>
2024-08-28 16:52:47 +0200

    Ajout d'un peu de style sur le layout default

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,7 @@
-<div>
-    <a href="/">Laravel Forum</a>
-</div>
-<nav>
-    
+<nav class="bg-red-400 flex justify-center">
+    <ul class="flex justify-between font-medium p-3 w-max">
+        <li><a href="/" class="text-gray-100 hover:bg-red-500 p-3 rounded-lg">Home</a></li>
+        <li><a href="{{ route('topic.index') }}" class="text-gray-100 hover:bg-red-500 p-3 rounded-lg">Topics</a></li>
+        <li><a href="" class="text-gray-100 hover:bg-red-500 p-3 rounded-lg">Categories</a></li>
+    </ul>
 </nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,14 +5,14 @@
     </head>
 
     <body>
-        <main>
+        <main class="flex flex-col h-screen">
             <header>
                 @include('includes.header')
             </header>
-            <div>
+            <div class="mb-auto px-20 py-5">
                 @yield('content')
             </div>
-            <footer>
+            <footer class="flex justify-center p-3 w-full bg-red-400 text-gray-100 place-self-end">
                 @include('includes.footer')
             </footer>
         </main>
